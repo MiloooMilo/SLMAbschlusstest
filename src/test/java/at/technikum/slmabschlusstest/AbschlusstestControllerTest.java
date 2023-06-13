1e0de242ce5f565cbbce0efd946f36592a429b19
Milorad <wi22b002@technikum-wien.at>
2023-06-13 10:11:20 +0200

    this commit will add the digitsum testfile

---
+++ src/test/java/at/technikum/slmabschlusstest/AbschlusstestControllerTest.java
@@ -0,0 +1,16 @@
+package at.technikum.slmabschlusstest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbschlusstestControllerTest {
+
+    int n = 22;
+
+    @Test
+    void testDigitSum(){
+        assertEquals(AbschlusstestController.calculateDigitSum(n),4);
+    }
+
+}
